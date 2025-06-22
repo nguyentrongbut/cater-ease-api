7692e5b452a70ab3202f83eb04e684e9e9fd50cc
nguyentrongbut <cloly031128@gmail.com>
2025-06-22 20:06:55 +0700

    fix(model):validate name phone email password in auth

--- Models/AuthModel.cs
+++ Models/AuthModel.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace cater_ease_api.Models;
@@ -9,11 +10,22 @@ public class AuthModel
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email is required")]
     public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Password is required")]
     public string Password { get; set; } = null!;
+
     public string Role { get; set; } = "customer";
+
     public string? Avatar { get; set; }
-    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Phone is required")]
+    public string Phone { get; set; } = null!;
+
     public string? Address { get; set; }
     public string Status { get; set; } = "active";
 }
