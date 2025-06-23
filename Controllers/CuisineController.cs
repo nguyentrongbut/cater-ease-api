de790b8cb8ab46dda2ba7b6b10342f9b832a5a46
nguyentrongbut <cloly031128@gmail.com>
2025-06-23 08:41:51 +0700

    feat(cuisine): crud cuisine

---
+++ Controllers/CuisineController.cs
@@ -0,0 +1,82 @@
+using cater_ease_api.Data;
+using cater_ease_api.Dtos.Cuisine;
+using cater_ease_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace cater_ease_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuisineController : ControllerBase
+    {
+        private readonly IMongoCollection<CuisineModel> _cuisines;
+
+        public CuisineController(MongoDbService mongoDbService)
+        {
+            _cuisines = mongoDbService.Database.GetCollection<CuisineModel>("cuisines");
+        }
+// [GET] api/cuisine
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _cuisines.Find(_ => true).ToListAsync();
+            return Ok(data);
+        }
+
+        // [GET] api/cuisine/:id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var dish = await _cuisines.Find(d => d.Id == id).FirstOrDefaultAsync();
+            return dish == null ? NotFound() : Ok(dish);
+        }
+        
+        // [POST] api/cuisine
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCuisineDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var model = new CuisineModel
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+            };
+
+            await _cuisines.InsertOneAsync(model);
+            return Ok(model);
+        }
+
+        // [PATCH] api/cuisine/:id
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Patch(string id, [FromBody] UpdateCuisineDto dto)
+        {
+            var updates = new List<UpdateDefinition<CuisineModel>>();
+    
+            if (dto.Title != null)
+                updates.Add(Builders<CuisineModel>.Update.Set(e => e.Title, dto.Title));
+
+            if (dto.Description != null)
+                updates.Add(Builders<CuisineModel>.Update.Set(e => e.Description, dto.Description));
+
+            if (updates.Count == 0)
+                return BadRequest("No valid fields provided.");
+
+            var update = Builders<CuisineModel>.Update.Combine(updates);
+            var result = await _cuisines.UpdateOneAsync(e => e.Id == id, update);
+
+            return result.MatchedCount == 0
+                ? NotFound()
+                : Ok("Cuisine updated successfully.");
+        }
+        
+        // [DELETE] api/cuisine/:id
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await _cuisines.DeleteOneAsync(e => e.Id == id);
+            return result.DeletedCount == 0 ? NotFound() : Ok("Deleted cuisine successfully.");
+        }
+    }
+}
---
+++ Dtos/Cuisine/UpdateCuisineDto.cs
@@ -0,0 +1,8 @@
+﻿namespace cater_ease_api.Dtos.Cuisine;
+
+public class UpdateCuisineDto
+{
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+}
